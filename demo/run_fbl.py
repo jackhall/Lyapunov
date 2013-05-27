13046368a7d3124ab8dba5766faaf24853cffabb
jackhall <jackwhall7@gmail.com>
2013-05-27 01:20:46 -0500

    started plotting tools

--- demo/run_fbl.py
+++ demo/run_fbl.py
@@ -19,9 +19,38 @@
 #The author may be reached at jackhall@utexas.edu.
 
 import time
+import matplotlib.pyplot as plt
+from itertools import izip
 import lyapunov
 #import solvers
 import motor_demo
+
+
+class Plotter(object):
+	def __init__(self, output_labels, state_labels):
+		self.output_labels = ['reference angle', 'filtered reference',
+							  'motor angle']
+		self.state_labels = ['stator current', 'rotor current', 
+							 'angular velocity', 'angle']
+
+	def plot_outputs(self):
+		plt.figure()
+		for i, ilabel in enumerate(output_labels):
+			plt.plot(self.t_out, self.y_out[:,i], label=ilabel)
+		plt.xlabel("time (s)")
+		plt.title("states")
+		plt.legend()
+		plt.show()
+
+	def plot_states(self):
+		plt.figure()
+		for i, ilabel in enumerate(state_labels):
+			plt.plot(self.t_out, self.x_out[:,i], label=ilabel)
+		plt.xlabel("time (s)")
+		plt.title("states")
+		plt.legend()
+		plt.show()
+
 
 step_time = 0.1
 #Construct subsystems.
@@ -40,24 +69,19 @@ plant.state = (1.0,)*4
 prefilter.state = (0.0,)*len(self.prefilter)
 sys = lyapunov.CompositeSystem([reference, prefilter, controller, plant])
 
-output_labels = ['reference angle', 'filtered reference',
-					  'motor angle']
 
 final_time = 8.0
 num_points = 1000
 print "\nMotor with Feedback Linearization, no observer"
 print "initial state:", sys.state
-
 #stepper = solvers.Stepper(sys)
 #stepper.step(0.01)
 #print "next state:", sys.state
-
 print "simulating for", final_time, "sec with", num_points, "points."
 start = time.clock()
 sol = lyapunov.Solver(sys, points=num_points)
-sys.x_out, sys.y_out, sys.t_out = sol.simulate(final_time)
+x_out, y_out, t_out = sol.simulate(final_time)
 print "final state:", sol.x_out[-1]
 print "elapsed time =", time.clock() - start
-#write plotting function
-sys.plot()
+plot_motor(t_out, x_out)
 
