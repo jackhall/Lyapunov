47b830062c89ce0ffca0b0a0797495cec8c05534
jackhall <jackwhall7@gmail.com>
2013-09-25 18:22:49 -0500

    new data, unwrapped angle

--- lyapunov/demo/pendubot_demo.py
+++ lyapunov/demo/pendubot_demo.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python
 
-import lyapunov
+import math
 import numpy
 from scipy.interpolate import interp1d
+import lyapunov
 
-pendubot_data_begin = {"pendubot_run1.dat": 210, 
-              "pendubot_run2.dat": 230,
-              "pendubot_run3.dat": 170}
-
-class PendubotReader(object):
+class Reader(object):
     def __init__(self, filename):
         data = numpy.loadtxt(filename, 
-                             skiprows=pendubot_data_begin[filename], 
                              usecols=range(5))
         rows = data.shape[0]
         self.time_data = numpy.linspace(0, rows*0.01, rows)
-        self.theta1 = interp1d(time_data, data[:,1], kind=cubic)
-        self.theta2 = interp1d(time_data, data[:,2], kind=cubic)
+        theta1_data = numpy.unwrap(data[:,1])
+        theta2_data = numpy.unwrap(data[:,2])
+        self.theta1 = interp1d(time_data, theta1_data, kind=cubic)
+        self.theta2 = interp1d(time_data, theta2_data, kind=cubic)
         #self.omega1 = interp1d(data[:,0], data[:,3], kind=cubic)
         #self.omega2 = interp1d(data[:,0], data[:,4], kind=cubic)
         self.state = 0.0, ()
@@ -27,11 +25,10 @@ class PendubotReader(object):
         return self.theta1(self.time), self.theta2(self.time)
 
 
-class PendubotObserver(object):
+class Observer(object):
     def __init__(self):
         self.state = 0.0, (0.0,)*6
-        self.events = 
-        self.y
+        self.y = None
        
     state = lyapunov.state_property()
 
@@ -40,15 +37,17 @@ class PendubotObserver(object):
 
 def run_pendubot_demo():
     filename = "pendubot_run1.dat"
-    reader = PendubotReader(filename)
-    observer = PendubotObserver(filename)
+    reader = Reader(filename)
+    observer = Observer(filename)
     pendubot = lyapunov.ParallelSystems([reader, observer])
     observer.y = reader.theta
     observer.state = 0.0, reader.theta + (0.0, 0.0) + (0.1, 0.1) 
 
+    record = lyapunov.Recorder(pendubot)
     stepper = lyapunov.adams_bashforth4(pendubot, reader.time_data)
-    for t, events in stepper:
-        if events:
-            
-    
+    for t, _ in stepper:
+        record.log()                        
+                   
+    print "b1 ~=", observer.state.x[-2]
+    print "b2 ~=", observer.state.x[-1]
 
