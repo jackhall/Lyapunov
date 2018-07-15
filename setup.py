55158f7439fb5020b0cc1670fc58c00e6290edd1
Jack Hall <jackwhall7@gmail.com>
2018-07-14 20:23:21 -0500

    switched to a BSD license from GPL

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from distutils.core import setup, Extension
 
-#boost 1.53 or later is required
-#make sure the boost install includes boost.python and boost.numeric.odeint
+# boost 1.53 or later is required
+# make sure the boost install includes boost.python and boost.numeric.odeint
 
 setup(name="lyapunov",
       version="3.0.1",
@@ -11,15 +11,14 @@ setup(name="lyapunov",
       author="Jack Hall",
       author_email="jackwhall7@gmail.com",
       url="https://github.com/jackhall/Lyapunov",
-      license="GNU-GPLv3",
-      data_files=[("", ["GNU-GPLv3", "README.md"])],
-      requires=["numpy", "matplotlib"], #not sure which versions are ok
+      license="BSD-3-Clause",
+      data_files=[("", ["BSD-3-Clause.lic", "README.md"])],
+      requires=["numpy", "matplotlib"],  # not sure which versions are ok
       packages=["lyapunov", "lyapunov.demo"],
-      ext_modules=[Extension("lyapunov.solvers", 
+      ext_modules=[Extension("lyapunov.solvers",
                              ["lyapunov/solvers.cpp"],
-                             include_dirs=["include"], 
+                             include_dirs=["include"],
                              libraries=["boost_python", "boost_system"],
                              extra_compile_args=["-std=c++11"],
                              )],
       )
-
